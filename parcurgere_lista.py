4423519f6cd47d17b6b4bcb40d53a3a467118941
flaviavls <122861285+flaviavls@users.noreply.github.com>
2023-01-17 10:09:54 +0200

    Update parcurgere_lista.py

--- parcurgere_lista.py
+++ parcurgere_lista.py
@@ -34,7 +34,35 @@ def suma_litere():
         suma += dic1[cu[i]]
     print('Suma este:', suma)
 
+def crypt_hash(propozitie):
+    dict3crypt = {'a':'d', 'b':'e', 'c':'f', 'd':'g', 'e':'h', 'f':'i', 'g':'j',
+                  'h':'k', 'i':'l', 'j':'m', 'k':'n', 'l':'o', 'm':'p', 'n':'q',
+                  'o':'r', 'p':'s', 'q':'t', 'r':'u', 's':'v', 't':'w', 'u':'x',
+                  'v':'y', 'w':'z', 'x':'a', 'y':'b', 'z':'c'}
+    rezultat = ''
+    for element in propozitie:
+        if element in ('', ',', '-', '1','2', '3', '4', '5', '6', '7', '8', '9', '0', '?'):
+            rezultat += dict3crypt[element]
+        return rezultat
+
+
+def decrypt_hash(propozitie):
+    dict3crypt = {'d':'a', 'e':'b', 'f':'c', 'g':'d', 'h':'e', 'i':'f', 'j':'g', 'k':'h',
+                  'l':'i', 'm':'j', 'n':'k', 'o':'l', 'p':'m', 'q':'n', 'r':'o', 's':'p',
+                  't':'q', 'u':'r', 'v':'s', 'w':'t', 'x':'u', 'y':'v', 'z':'w', 'a':'x',
+                  'b':'y', 'c':'x'}
+    rezultat = ''
+    for element in propozitie:
+        if element in ('', ',', '-', '1','2', '3', '4', '5', '6', '7', '8', '9', '0', '?'):
+            rezultat += element
+        else:
+            rezultat += dict3crypt[element]
+    return rezultat
+
+
 
 if __name__ == "__main__":
     #parcurgere_lista()
     (parcurgere_lista1())
+    suma_litere()
+    decrypt_hash()
