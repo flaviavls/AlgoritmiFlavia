72a8b531f38daff1f9241788b9f558b54c1dd412
flaviavls <122861285+flaviavls@users.noreply.github.com>
2023-01-17 09:47:11 +0200

    Update parcurgere_lista.py

--- parcurgere_lista.py
+++ parcurgere_lista.py
@@ -13,6 +13,27 @@ def parcurgere_lista1():
     for i in range(len(l1)):
         print(l1[i])
 
+def lista_abc():
+    l1 = list()
+    a = 10
+    b = 20
+    c = 30
+    l1 = [a+b+c]
+    for element in l1:
+        print(element)
+    for i in range(len(l1)):
+        print(l1[i])
+
+def suma_litere():
+    # definessc dictionarul/ hash table
+    # sub forma de mai jos
+    dic1 = {'a':10, 'b':20, 'c':30}
+    suma = 0
+    cu = "abc"
+    for i in range (len(cu)):
+        suma += dic1[cu[i]]
+    print('Suma este:', suma)
+
 
 if __name__ == "__main__":
     #parcurgere_lista()
